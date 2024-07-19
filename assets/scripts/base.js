fd5481e71be489a52d7f04e389bd85e0d9e1b39f
SoRaang <dsc0320@hotmail.com>
2024-07-19 14:37:16 +0900

    Update

--- assets/scripts/base.js
+++ assets/scripts/base.js
@@ -1,5 +1,6 @@
 const inputBirth = document.getElementById('dtBirth');
 const btnCheck = document.getElementById('btnBirth');
+const printResult = document.getElementById('birthCheck');
 
 function checkBirth(dateValue) {
     let thisYear = new Date().getFullYear();
@@ -7,12 +8,19 @@ function checkBirth(dateValue) {
 
     let yourAge = thisYear - birthYear;
 
+    printResult.style = '';
+
     if (!!dateValue === false) {
-        alert('날짜가 입력되지 않았습니다.');
+        printResult.innerHTML = '날짜가 입력되지 않았습니다.';
+        printResult.style.backgroundColor = '#a83034';
     } else if (yourAge < 0) {
-        alert('아직 안 태어나셨네요');
+        printResult.innerHTML = '아직 안 태어나셨네요';
+        printResult.style.backgroundColor = '#322460';
+        printResult.style.color = 'white';
     } else {
-        alert(yourAge + '살 이십니다!');
+        printResult.innerHTML = yourAge + '살 이십니다!';
+        printResult.style.backgroundColor = '#00a0de';
+        printResult.style.color = 'white';
     }
 }
 
